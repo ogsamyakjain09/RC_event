ac49f09a6a9bc880aa4ca96b036b2cae12ce318f
ogsamyakjain09 <work.samyakjain9@gmail.com>
2026-05-28 15:22:56 +0530

    use env variable for pocketbase url

--- src/pocketbase.ts
+++ src/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase(import.meta.env.VITE_PB_URL || 'http://127.0.0.1:8090');
 
 // Keep auth token in sync
 pb.authStore.onChange(() => {
